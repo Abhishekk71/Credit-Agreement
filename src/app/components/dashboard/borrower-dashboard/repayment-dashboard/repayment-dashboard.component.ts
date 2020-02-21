880bffa067d27eb046d410c1da0e20afe43ee30f
TingkaiHu <hu.tin@husky.neu.edu>
2020-02-21 01:40:40 -0500

    get loan application data from local-storage

--- src/app/components/dashboard/borrower-dashboard/repayment-dashboard/repayment-dashboard.component.ts
+++ src/app/components/dashboard/borrower-dashboard/repayment-dashboard/repayment-dashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { Router } from '@angular/router';
+import { LocalStorageService } from './../../../../services/local-storage.service';
 // import { Web3Service } from 'src/app/services/web3.service';
 
 @Component({
@@ -10,7 +11,7 @@ import { Component, OnInit } from '@angular/core';
 export class RepaymentDashboardComponent implements OnInit {
 
   userAddress = "";
-
+  loanApplications = [];
   contranctToPay = [];
   Digest_show_hide_val=true;
 
@@ -18,17 +19,50 @@ export class RepaymentDashboardComponent implements OnInit {
     1,2,3,4,5
   ]
 
-  constructor() { }
+  constructor(private router: Router,
+    private localStorageService: LocalStorageService) { }
   // private localStorageService: LocalStorageService,
   // private web3Service: Web3Service
 
   ngOnInit() {
     console.log("inside repayment dashboard");
-    
+    this.getUserAddress();
+    this.getApplications();
+  }
+
+  getUserAddress(){
+    let userAddress = this.localStorageService.getUser();
+    if (userAddress == null) {
+      alert("Invalid user session, please login again..");
+      this.router.navigateByUrl('/login');
+      return;
+    }
+    this.userAddress = userAddress;
+  }
+
+  getApplications() {
+    if(this.localStorageService.getLoanApplications()){
+      let loanApplications = this.localStorageService.getLoanApplications();
+      for (let application of loanApplications){
+        if (application.borrower.address == this.userAddress){
+          console.log(application.name);          
+          this.loanApplications.push(application)
+        }
+      }
+    }
+    else{
+      alert("cannot find local storage file");
+      this.router.navigateByUrl('/login');
+      return;
+    }
   }
 
   Digest_show=function(){
       this.Digest_show_hide_val=!this.Digest_show_hide_val;
+  }
+
+  back(){
+    this.router.navigateByUrl("borrower");
   }
 
 }
