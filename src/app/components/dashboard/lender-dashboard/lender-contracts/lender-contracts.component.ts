3c27f34d3d26733cf23c90233bf28fcbaa69ea90
HandanGu <gu.ha@husky.neu.edu>
2020-02-24 16:06:58 -0500

    after lender's sign, deploy facility and send USDCoin from lender to borrower && add starting date in borrower's create application and calculate its ending date

--- src/app/components/dashboard/lender-dashboard/lender-contracts/lender-contracts.component.ts
+++ src/app/components/dashboard/lender-dashboard/lender-contracts/lender-contracts.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router,ActivatedRoute,Params } from '@angular/router';
+import { Router, ActivatedRoute, Params } from '@angular/router';
 import { LocalStorageService } from '../../../../services/local-storage.service';
 import { Web3Service } from '../../../../services/web3.service';
 import { ContractService } from './../../../../services/contract.service';
@@ -14,29 +14,29 @@ const usd_coin_artifacts = require('./../../../../../../build/contracts/USDCoin.
   styleUrls: ['./lender-contracts.component.css']
 })
 export class LenderContractsComponent implements OnInit {
-  applications=[];
+  applications = [];
   userAddress = "";
-  lendAmount=0;
-  
+  lendAmount = 0;
+
   account = {};
   accountBalance = 0;
 
   USDCoin: any;
   accounts = [];
-  id=0;
- 
-  constructor(private router: Router, 
-    private localStorageService : LocalStorageService, 
+  id = 0;
+
+  constructor(private router: Router,
+    private localStorageService: LocalStorageService,
     private web3Service: Web3Service,
     private contractService: ContractService,
-    public activeRoute:ActivatedRoute) { }
+    public activeRoute: ActivatedRoute) { }
 
   async ngOnInit() {
 
-    
 
-    
-    
+
+
+
     //get user
     let userAddress = this.localStorageService.getUser();
     if (userAddress == null) {
@@ -59,21 +59,45 @@ export class LenderContractsComponent implements OnInit {
 
     //get applications
     let loanApplications = this.localStorageService.getLoanApplications();
+    var deployedContract;
+    var deployedFacility;
     for (let application of loanApplications) {
       for (let detail of application.lenderDetails) {
         if (detail.lender.address == this.userAddress) {
-          console.log("in the loop");
-          console.log(application);
+          await this.contractService.getDeployedContract('CreditAgreement', application.aggreementAddress)
+            .then(async (_deployedContract) => {
+              deployedContract = _deployedContract;
+              var facilityAddress = await deployedContract.getFacility(this.userAddress, { from: this.userAddress });
+              console.log(" facilityAddress is: ", facilityAddress);
+              await this.contractService.getDeployedContract('Facility', facilityAddress)
+                .then(async (_deployedFacility) => {
+                  deployedFacility = _deployedFacility;
+                  console.log("deployedFacility is: ");
+                  console.log(deployedFacility);
+                  const value = await deployedFacility.getInfo({ from: this.userAddress });
+                  application['maturityDate'] = this.timestampToTime(value[2]);
+                  application['expiryDate'] = this.timestampToTime(value[3]);
+                });
+            });
           this.applications.push(application);
         }
       }
     }
-
-    console.log("applications are");
-    console.log(this.applications);
   }
 
-  sum(application){
+  timestampToTime(timestamp) {
+    var date = new Date(1398250549490);
+    var Y = date.getFullYear() + '-';
+    var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    var D = date.getDate() + ' ';
+    var h = date.getHours() + ':';
+    var m = date.getMinutes() + ':';
+    var s = date.getSeconds(); 
+    console.log(Y + M + D + h + m + s);
+    return (Y + M + D + h + m + s);
+  }
+
+  sum(application) {
     let sum = 0;
     for (let detail of application.lenderDetails) {
       if (detail.lender.address == this.userAddress) {
@@ -82,23 +106,4 @@ export class LenderContractsComponent implements OnInit {
     }
     return sum;
   }
-
-  // getMaturityDate(application) {
-  //   var deployedAgreementContract;
-  //   await this.contractService.getDeployedContract('CreditAgreement', this.application.aggreementAddress)
-  //     .then(async (deployedContract) => {
-  //       deployedAgreementContract = deployedContract;
-  //       console.log("deployedAgreementContract is:");
-  //       console.log(deployedAgreementContract);
-  //       console.log("test sign from: ", this.userAddress);
-  //       await deployedAgreementContract.signAsALender({ from: this.userAddress }).then(data => console.log(data));
-  //       console.log("check sign! ", await deployedAgreementContract.check({ from: this.userAddress }));
-  //       console.log("check everyontsigned: ", await deployedAgreementContract.hasEveryoneSigned({ from: this.userAddress }));
-  // }
-
-  
-
-
 }
-
-
